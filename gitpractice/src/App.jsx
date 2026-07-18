251caeda9511ae6ad93eff7516d744c4aeb3ec45
Pruthvi <pruthvikonda575@gmail.com>
2026-07-18 15:55:25 +0530

    login is added

--- gitpractice/src/App.jsx
+++ gitpractice/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
+import Login from './Components/Login'
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Login />
     </div>
   )
 }
